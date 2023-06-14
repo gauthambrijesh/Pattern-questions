5aa99b316ebfb39fb7c25efc1601801f32d3838f
Gautham Brijesh <93137193+gauthambrijesh@users.noreply.github.com>
2023-06-14 17:05:09 +0530

    Update Hollow Rectangle.py

--- Hollow Rectangle.py
+++ Hollow Rectangle.py
@@ -1,7 +1,7 @@
-rows = 5
-cols = 5
-for i in range(rows):
-    for j in range(cols):
+rows = 6
+cols = 6
+for i in range(1,rows):
+    for j in range(1,cols):
         if i == 0 or i == rows - 1 or j == 0 or j == cols - 1:
             print(i, end=" ")
         else:
